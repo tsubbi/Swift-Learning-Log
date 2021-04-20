461ee3c76b5b7da1ff988e985c1aacbff5c99156
Jamie <jamie.yjchen@gmail.com>
2021-04-20 10:44:26 -0700

    complete lab: function

--- Labs/Student/Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
+++ Labs/Student/Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
@@ -8,12 +8,42 @@
  A reoccurring process like this is a perfect candidate for a function. Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. Call the function multiple times and observe the printouts.
  */
 var steps = 0
-
+func incrementSteps() {
+    steps += 1
+//    print(steps)
+}
 
 /*:
  Similarly, if you want to regularly provide progress updates to your user, you can put your control flow statements that check on progress into a function. Write a function called `progressUpdate` after the declaration of `goal` below. The function should print "You're off to a good start." if `steps` is less than 10% of `goal`, "You're almost halfway there!" if `steps` is less than half of `goal`, "You're over halfway there!" if `steps` is less than 90% of `goal`, "You're almost there!" if `steps` is less than `goal`, and "You beat your goal!" otherwise. Call the function and observe the printout. Remember, you can convert numbers using the appropriate Int or Double initializer.
  */
 let goal = 10000
 
+func progressUpdate() {
+    let excersisedSteps = Int.random(in: 0...goal/2)
+    steps += excersisedSteps
+    
+    let tenthOfGoal = goal/10
+    let halfOfGoal = goal/2
+    let nintyPercentOfGoal = Int(Double(goal) * 0.9)
+    
+    switch steps {
+    case 0..<tenthOfGoal:
+        print("You're off to a good start")
+    case tenthOfGoal..<halfOfGoal:
+        print("You're almost halfway there!")
+    case halfOfGoal..<nintyPercentOfGoal:
+        print("You're over halfway there!")
+    case nintyPercentOfGoal..<goal:
+        print("You're almost there!")
+    case goal..<Int.max:
+        print("You beat your goal")
+    default:
+        break
+    }
+}
 
+progressUpdate()
+progressUpdate()
+progressUpdate()
+progressUpdate()
 //: [Previous](@previous)  |  page 2 of 6  |  [Next: Exercise - Parameters and Argument Labels](@next)
--- Labs/Student/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ Labs/Student/Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -3,11 +3,17 @@
 
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Dan" the return value might be "Hi, Dan! How are you?" Use the function and print the result.
  */
+func greet(name: String) -> String {
+    "Hi, \(name)"
+}
 
-
+print(greet(name: "John"))
 /*:
  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
  */
+func almostAddition(_ num1: Int, by num2: Int) -> Int {
+    num1+num2-2
+}
 
-
+print(almostAddition(5, by: 4))
 //: [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
